261816641f823d4c575e97c91982cab9361cfba6
Anarevian <srokajoerg@gmail.com>
2019-11-15 16:54:00 +0100

    Finished Player Controller(for Now)

---
+++ BA PROJECT - Hannah Pollow/Assets/Scenes/Scripts (Not Finished)/CameraController.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float minYAngle;
+    [SerializeField] private float maxYAngle;
+
+    [SerializeField] private string mouseXAxisName;
+    [SerializeField] private string mouseYAxisName;
+
+    [SerializeField] private float offset = -3;
+    [SerializeField] private float farthestZoom = -7;
+    [SerializeField] private float closestZoom = -2;
+    [SerializeField] private float camFollow = 8;
+    [SerializeField] private float camZoom = 1.75f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+
+    Camera myCamera;
+    LayerMask mask;
+
+    private void Start()
+    {
+        myCamera = gameObject.GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        mask = 1 << LayerMask.NameToLayer("Clippable") | 0 << LayerMask.NameToLayer("NotClippable");
+    }
+
+    private void Update()
+    {
+        x += Input.GetAxis(mouseXAxisName) * mouseSensitivity;
+        y += Input.GetAxis(mouseYAxisName) * mouseSensitivity;
+        y = Mathf.Clamp(y, minYAngle, maxYAngle);               
+    }
+
+    private void LateUpdate()
+    {
+        cameraMovement();
+        Zoom();
+    }
+
+    private void cameraMovement()
+    {
+        target.rotation = Quaternion.Lerp(player.transform.rotation, Quaternion.Euler(-y, x, 0), 0.5f);
+        player.transform.rotation = Quaternion.Lerp(player.transform.rotation, Quaternion.Euler(0, x, 0), 0.5f);
+
+        transform.LookAt(target);
+    }
+
+    private void Zoom()
+    {
+        offset += Input.GetAxis("Mouse ScrollWheel") * camZoom;
+
+        if (offset > closestZoom) offset = closestZoom;
+        else if (offset < farthestZoom) offset = farthestZoom;
+
+        float unobstructed = offset;
+        Vector3 idealPostion = target.TransformPoint(Vector3.forward * offset);
+
+        RaycastHit hit;
+        if (Physics.Linecast(target.position, idealPostion, out hit, mask.value))
+        {
+            unobstructed = -hit.distance + .01f;
+        }
+
+
+        Vector3 desiredPos = target.TransformPoint(Vector3.forward * unobstructed);
+        Vector3 currentPos = myCamera.transform.position;
+
+        Vector3 goToPos = new Vector3(Mathf.Lerp(currentPos.x, desiredPos.x, camFollow), Mathf.Lerp(currentPos.y, desiredPos.y, camFollow), Mathf.Lerp(currentPos.z, desiredPos.z, camFollow));
+
+        myCamera.transform.position = goToPos;
+        myCamera.transform.LookAt(target.position);
+
+
+        float c = myCamera.nearClipPlane;
+        bool clip = true;
+        while (clip)
+        {
+            Vector3 pos1 = myCamera.ViewportToWorldPoint(new Vector3(0, 0, c));
+            Vector3 pos2 = myCamera.ViewportToWorldPoint(new Vector3(.5f, 0, c));
+            Vector3 pos3 = myCamera.ViewportToWorldPoint(new Vector3(1, 0, c));
+            Vector3 pos4 = myCamera.ViewportToWorldPoint(new Vector3(0, .5f, c));
+            Vector3 pos5 = myCamera.ViewportToWorldPoint(new Vector3(1, .5f, c));
+            Vector3 pos6 = myCamera.ViewportToWorldPoint(new Vector3(0, 1, c));
+            Vector3 pos7 = myCamera.ViewportToWorldPoint(new Vector3(.5f, 1, c));
+            Vector3 pos8 = myCamera.ViewportToWorldPoint(new Vector3(1, 1, c));
+
+            Debug.DrawLine(myCamera.transform.position, pos1, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos2, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos3, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos4, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos5, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos6, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos7, Color.yellow);
+            Debug.DrawLine(myCamera.transform.position, pos8, Color.yellow);
+
+            if (Physics.Linecast(myCamera.transform.position, pos1, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos2, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos3, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos4, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos5, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos6, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos7, out hit, mask.value))
+            {
+
+            }
+            else if (Physics.Linecast(myCamera.transform.position, pos8, out hit, mask.value))
+            {
+
+            }
+            else clip = false;
+
+            if (clip) myCamera.transform.localPosition += myCamera.transform.forward * c;
+        }
+    }
+}
+
--- BA PROJECT - Hannah Pollow/Assets/Scenes/Scripts (Not Finished)/PlayerController.cs
+++ BA PROJECT - Hannah Pollow/Assets/Scenes/Scripts (Not Finished)/PlayerController.cs
@@ -4,49 +4,92 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private byte movSpeedMult;
-    [SerializeField] private byte flySpeedMult;
-    [SerializeField] private byte jumpForceMult;
+    [SerializeField] private float walkSpeedMult;
+    [SerializeField] private float runSpeedMult;
+    [SerializeField] private float flySpeedMult;
+    [SerializeField] private float jumpForceMult;
+    [SerializeField] private float gravityMult;
 
     [SerializeField] private string horizAxisName;
     [SerializeField] private string vertAxisName;
     [SerializeField] private string jumpAxisName;
+    [SerializeField] private string sprintAxisName;
+    [SerializeField] private string flyToggleAxisName;
+    [SerializeField] private string flyDownAxisName;
 
     [SerializeField] private float groundCheckDist;
 
     [SerializeField] private AnimationCurve jumpFallof;
 
-    private int fwdangle;
-    private int bckangle;
-    private int rigangle;
-    private int lefangle;
+    private bool canFly;
 
     public void Start()
     {
+        canFly = false;
         groundCheckDist += GetComponent<Collider>().bounds.extents.y;
     }
 
     public void Update()
     {
-        Move();      
+        if(Input.GetAxis(flyToggleAxisName) == 1)
+        {
+            canFly = !canFly;
+        }
+
+        if(canFly)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            Fly();
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            Move();
+        }             
     }
 
+    public void Fly()
+    {
+        Vector3 movVec = new Vector3(Input.GetAxis(horizAxisName), 0, Input.GetAxis(vertAxisName));
+
+        if (Input.GetAxis(jumpAxisName) == 1)
+        {
+            movVec += Vector3.up;
+        }
+        else if(Input.GetAxis(flyDownAxisName) == 1)
+        {
+            movVec -= Vector3.up;
+        }    
+        
+        Vector3.ClampMagnitude(movVec, 1);
+
+        movVec *= flySpeedMult;
+
+        gameObject.transform.Translate(movVec * Time.deltaTime);
+    }
 
     public void Move()
     {
-        if((Input.GetAxis(horizAxisName) != 0 || Input.GetAxis(vertAxisName) != 0) && !IsOnSlope())
-        {
-            Vector3 movVec = Vector3.ClampMagnitude(new Vector3(Input.GetAxis(horizAxisName), 0, Input.GetAxis(vertAxisName)), 1);
-            movVec *= movSpeedMult * Time.deltaTime;
-            gameObject.transform.Translate(movVec);
-            if(Input.GetAxis(jumpAxisName) != 0 && IsGrounded())
-            {
-                Jump();
-            }
-        }
-        if(IsOnSlope())
-        {
 
+        Vector3 movVec = Vector3.ClampMagnitude(new Vector3(Input.GetAxis(horizAxisName), 0, Input.GetAxis(vertAxisName)), 1);
+
+        switch(Input.GetAxis(sprintAxisName))
+        {
+            case 1: movVec *= runSpeedMult * Time.deltaTime;
+                break;
+            default: movVec *= walkSpeedMult * Time.deltaTime;
+                break;
+        }
+
+        if(!IsGrounded())
+        {
+            movVec += Vector3.down * gravityMult * Time.deltaTime;
+        }
+        
+        gameObject.transform.Translate(movVec);
+        if (Input.GetAxis(jumpAxisName) != 0 && IsGrounded())
+        {
+            Jump();
         }
     }
 
@@ -54,20 +97,6 @@ public class PlayerController : MonoBehaviour
     {
         StartCoroutine(jumpEvaluation());
     }
-
-    public Vector3 SlopeSliding()
-    {
-        RaycastHit hit;
-
-        if(Physics.Raycast(transform.position, -Vector3.up, out hit, groundCheckDist + (groundCheckDist / 2)))
-        {
-            return Vector3.zero - hit.normal;
-        }
-        return Vector3.zero;
-       
-    }
-
-
 
     private IEnumerator jumpEvaluation()
     {
@@ -89,33 +118,10 @@ public class PlayerController : MonoBehaviour
     {
         //  Debug.DrawRay(transform.position, -transform.up * groundCheckDist, Color.red);
 
-        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, transform.position.z + (GetComponent<Collider>().bounds.extents.z / 4)), -transform.up * groundCheckDist);
-        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, transform.position.z - (GetComponent<Collider>().bounds.extents.z / 4)), -transform.up * groundCheckDist);
-        Debug.DrawRay(new Vector3(transform.position.x + (GetComponent<Collider>().bounds.extents.x / 4), transform.position.y, transform.position.z), -transform.up * groundCheckDist);
-        Debug.DrawRay(new Vector3(transform.position.x - (GetComponent<Collider>().bounds.extents.x / 4), transform.position.y, transform.position.z), -transform.up * groundCheckDist);
-
-        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + (GetComponent<Collider>().bounds.extents.z / 4)), -Vector3.up, groundCheckDist) ||
-            Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - (GetComponent<Collider>().bounds.extents.z / 4)), -Vector3.up, groundCheckDist) ||
-            Physics.Raycast(new Vector3(transform.position.x + (GetComponent<Collider>().bounds.extents.x / 4), transform.position.y, transform.position.z), -Vector3.up, groundCheckDist) ||
-            Physics.Raycast(new Vector3(transform.position.x - (GetComponent<Collider>().bounds.extents.x / 4), transform.position.y, transform.position.z), -Vector3.up, groundCheckDist))
+        if (Physics.Raycast(transform.position,-transform.up, groundCheckDist))
         {
             return true;
         }
         return false;
     }
-
-    public bool IsOnSlope()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, groundCheckDist + (groundCheckDist / 2)))
-        {
-            if (hit.normal != Vector3.up)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
