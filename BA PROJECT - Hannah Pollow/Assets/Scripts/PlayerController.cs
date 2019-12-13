9d5859f7c7fd241f7116d355e2a922bb8716a5bd
Anarevian <srokajoerg@gmail.com>
2019-12-13 13:01:36 +0100

    Player Controller Update
    Player Controller Updated with new Flying Mechanics

---
+++ BA PROJECT - Hannah Pollow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeedMult;
+    [SerializeField] private float runSpeedMult;
+    [SerializeField] private float flySpeedMult;
+    [SerializeField] private float jumpForceMult;
+    [SerializeField] private float gravityMult;
+
+    [SerializeField] private string horizAxisName;
+    [SerializeField] private string vertAxisName;
+    [SerializeField] private KeyCode jumpAxisName;
+    [SerializeField] private string sprintAxisName;
+    [SerializeField] private string flyDownAxisName;
+
+    [SerializeField] private float groundCheckDist;
+
+    [SerializeField] private AnimationCurve jumpFallof;
+    [SerializeField] private float timeBetweenJump;
+
+    [SerializeField] private Animator anim;
+    [SerializeField] private int maxJumps;
+
+    public int jumpCount = 0;
+    private bool isjumping;
+
+
+    public void Start()
+    {
+        groundCheckDist += (GetComponent<Collider>().bounds.extents.y / 2);
+    }
+
+    public void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+        Vector3 movVec = Vector3.ClampMagnitude(new Vector3(Input.GetAxis(horizAxisName), 0, Input.GetAxis(vertAxisName)), 1);
+
+        switch (Input.GetAxis(sprintAxisName))
+        {
+            case 1:
+                movVec *= runSpeedMult * Time.deltaTime;
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("IsIdleing", false);
+                break;
+            default:
+                movVec *= walkSpeedMult * Time.deltaTime;
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("IsIdleing", false);
+                break;
+        }
+
+        if (Input.GetAxis(horizAxisName) == 0 && Input.GetAxis(vertAxisName) == 0)
+        {
+            anim.SetBool("IsIdleing", true);
+        }
+
+        if (!IsGrounded())
+        {
+            movVec += Vector3.down * gravityMult * Time.deltaTime;
+        }
+
+        gameObject.transform.Translate(movVec);
+        if (Input.GetKeyDown(jumpAxisName) && jumpCount < maxJumps)
+        {
+            anim.SetBool("isFlying", true);
+            anim.SetBool("IsIdleing", false);
+            Jump();
+            jumpCount++;
+        }
+        else if (jumpCount >= maxJumps && IsGrounded())
+        {
+            jumpCount = 0;
+        }
+
+
+    }
+
+    public void Jump()
+    {
+        StartCoroutine(jumpEvaluation());
+    }
+
+    private IEnumerator jumpEvaluation()
+    {
+        isjumping = true;
+        float airTime = 0.0f;
+        float evaluation = 0.0f;
+        do
+        {
+            evaluation = jumpFallof.Evaluate(airTime);
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, evaluation * jumpForceMult, 0));
+            airTime += Time.deltaTime;
+            yield return null;
+        }
+        while (airTime <= timeBetweenJump);
+        isjumping = false;
+        yield break;
+    }
+
+
+    public bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -transform.up * groundCheckDist, Color.red);
+
+        if (Physics.Raycast(transform.position, -transform.up, groundCheckDist))
+        {
+            anim.SetBool("IsGrounded", true);
+            if (!isjumping)
+            {
+                anim.SetBool("isFlying", false);
+            }
+            return true;
+        }
+        anim.SetBool("IsGrounded", false);
+        return false;
+    }
+}
